655d5055821d2fea24805a47f1867ea48837a2dc
mvgv <mvgv1989@gmail.com>
2023-11-07 00:43:30 -0300

    feat: ajustando conta aws e redes para se comunicar com o eks

--- main.tf
+++ main.tf
@@ -1,48 +1,42 @@
-# Criacao da Subnet Group (Se ela ja for criada, ela dar um erro)
 resource "aws_docdb_subnet_group" "default" {
-  name       = "aws_documentdb_subnets_group"
-  subnet_ids = ["subnet-0c7b856b1f711f677", "subnet-0908e9ab2aa6b159e", "subnet-0f8438472afe65156", "subnet-038888a2be2ea6651", "subnet-03d0ba95451a0827a", "subnet-00d6542dcd387a9e6"]
+ name      = "aws_documentdb_subnets_group"
+ subnet_ids = ["subnet-09c9a33adaaf98d3e", "subnet-003f5b4052f947d83"] # your private subnet IDs
 }
 
-# Criacao do Security Group com suas regras!
 resource "aws_security_group" "this" {
-  name        = "security_group_documentdb_fastfood" # Nome do grupo de segurança
-  description = "Allow inbound traffic"
+ name       = "security_group_documentdb_fastfood"
+ description = "Allow inbound traffic"
 
-  vpc_id = "vpc-0e9b8ee3729fd95f0" # ID da sua VPC
+ vpc_id = "vpc-03529632a86648ad4" # your EKS VPC ID
 
-  # Inobound / Entrada
-  ingress {
-    from_port   = 27017 # Porta do DocumentDB
-    to_port     = 27017 # Porta do DocumentDB
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ ingress {
+  from_port = 27017
+  to_port   = 27017
+  protocol  = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
 
-  # Outbound / Saida
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ egress {
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
 }
 
-# Cria o cluster do DocumentDB
 resource "aws_docdb_cluster" "document_db" {
-  cluster_identifier     = "fastfood-docdb2-cluster"
-  engine                 = "docdb"
-  master_username        = var.db_username
-  master_password        = var.db_password
-  skip_final_snapshot    = true
-  db_subnet_group_name   = "aws_documentdb_subnets_group"
-  vpc_security_group_ids = ["sg-06474215984a0c702"]
+ cluster_identifier   = "fastfood-docdb2-cluster"
+ engine               = "docdb"
+ master_username      = var.db_username
+ master_password      = var.db_password
+ skip_final_snapshot  = true
+ db_subnet_group_name = "aws_documentdb_subnets_group"
+ vpc_security_group_ids = [aws_security_group.this.id]
 }
 
-# Cria a instancia do DocumentDB
 resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = 1
-  identifier         = "fastfood-db2-instance-${count.index}"
-  cluster_identifier = aws_docdb_cluster.document_db.id
-  instance_class     = "db.t3.medium"
+ count              = 1
+ identifier         = "fastfood-db2-instance-${count.index}"
+ cluster_identifier = aws_docdb_cluster.document_db.id
+ instance_class     = "db.t3.medium"
 }
