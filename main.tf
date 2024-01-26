6287a81c606c5cda169185fe6f466ec15a466e47
Marcus <mvgv1989@gmail.com>
2024-01-26 16:18:06 -0300

    chore: alterando dados do banco para o app de pedidos

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "aws_docdb_subnet_group" "default" {
- name      = "aws_bdocumentsdb_subnets_groups"
- subnet_ids = ["subnet-00284258966405cf4", "subnet-03ee8d741c1bf8645"] # your private subnet IDs
+ name      = "aws_appfastfood_subnets_groups"
+ subnet_ids = ["subnet-0df3426b76db09446", "subnet-06743e5d38fc5e51e"] # your private subnet IDs
 }
 
 resource "aws_security_group" "this" {
  name       = "security_group_documentdb_fastfoods"
  description = "Allow inbound traffic"
 
- vpc_id = "vpc-033482dbcf507bd17" # your EKS VPC ID
+ vpc_id = "vpc-0735ed8837ac86ea8" # your EKS VPC ID
 
  ingress {
   from_port = 27017
@@ -30,7 +30,7 @@ resource "aws_docdb_cluster" "document_db" {
  master_username      = var.db_username
  master_password      = var.db_password
  skip_final_snapshot  = true
- db_subnet_group_name = "aws_bdocumentsdb_subnets_groups"
+ db_subnet_group_name = "aws_appfastfood_subnets_group"
  vpc_security_group_ids = [aws_security_group.this.id]
 }
 
